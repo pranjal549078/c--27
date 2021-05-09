e26b4ae0f3eeabcc3afac83d239de68ef85170a5
@pranjal549078 <sadhnagaikwad0@gmail.com>
2021-05-09 15:07:26 +0530

    c-27

--- sketch.js
+++ sketch.js
@@ -4,73 +4,53 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1,box2,box3,box4,box5,box6,box7,box8,box9,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20;
-var ball
+ var bob1,bob2,bob3,bob4,bob5
+ var rope1,rope2,rope3,rope4,rope5
+ var block;
 function setup() {
   createCanvas(3000,800);
   engine = Engine.create();
   world = engine.world;
+
   ground = new Ground(600,600,1200,20);
-  box1 = new Box(900,100,70,70);
-  box2 = new Box(900,100,70,70) ; 
-  box3 = new Box(900,100,70,70);
-  box4 = new Box(900,100,70,70);
-  box5 = new Box(900,100,70,70);
-  box6 = new Box(900,100,70,70);
-  
-  box7 = new Box(800,100,70,70)
-  box8 = new Box(800,100,70,70)
-  box9 = new Box(800,100,70,70)
-  box10 = new Box(800,100,70,70)
-  box11 = new Box(800,100,70,70)
-  box12 = new Box(800,100,70,70)
-  
-  box13 = new Box(700,100,70,70)
-  box14 = new Box(700,100,70,70)
-  box15 = new Box(700,100,70,70)
-  box16 = new Box(700,100,70,70)
-  box17 = new Box(700,100,70,70)
-  box18 = new Box(700,100,70,70)
-  box19 = new Box(700,100,70,70)
-  box20 = new Box(700,100,70,70)
 
 //ball
-  ball = new Ball(200,200,80,80)
+  bob1 = new Bob(2,200,80,80)
+  bob2 = new Bob(300,200,80,80)
+  bob3 = new Bob(400,200,80,80)
+  bob4 = new Bob(500,200,80,80)
+  bob5 = new Bob(600,200,80,80)
+
 
   //rope
-  rope = new Rope(ball.body,{x:500,y:50} )
+  rope1 = new Rope(bob1.body,{x:200,y:50} )
+  rope2 = new Rope(bob2.body,{x:300,y:50} )
+  rope3 = new Rope(bob3.body,{x:400,y:50} )
+  rope4 = new Rope(bob4.body,{x:500,y:50} )
+  rope5 = new Rope(bob5.body,{x:600,y:50} )
 
+  block = createSprite(200,200,120,50);
 }
 function draw() {
+ 
+drawSprites();
   background(180);
   Engine.update(engine);  
-  ground.display();
+
+
+ ground.display();
   //ball
-  ball.display();
-
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
   //rope
-  rope.display();
-
-   box1.display();
-   box2.display();
-   box3.display();
-   box4.display();
-   box5.display();
-   box6.display();
-   box7.display();
-   box8.display();
-   box9.display();
-   box10.display();
-   box11.display();
-   box12.display();
-   box13.display();
-   box14.display();
-   box15.display();
-   box16.display();
-   box17.display();
-   box18.display();
-   box19.display();
-   box20.display();
+  rope1.display();
+  rope2.display(); 
+  rope3.display();  
+  rope4.display();  
+  rope5.display(); 
 }
 
 function mouseDragged()
